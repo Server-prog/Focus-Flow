9cd45d6491d0ed299b3436652d90586d7b5a1a97
João Tambue <tjoao7279@gmail.com>
2025-04-11 23:37:31 +0100

    novas atualizações

---
+++ src/pages/settings.tsx
@@ -0,0 +1,12 @@
+
+const Settings = () => {
+ 
+
+  return (
+    <div className="max-w-3xl mx-auto p-6 text-gray-800 dark:text-gray-100">
+      <h1>Definição</h1>
+    </div>
+  );
+}
+
+export default Settings;
